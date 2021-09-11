5dc2707888c5c4ae9567f542053967d181d5f6a7
Aziz-Sobirov <89915512+Aziz-Sobirov@users.noreply.github.com>
2021-09-11 17:44:42 +0500

    Add files via upload

--- js/as.js
+++ js/as.js
@@ -28,7 +28,7 @@ let navbar_mobile = document.getElementById("navbar_mobile")
 
 // desktop
 for(let i=0;i<navbar_list.length;i++){
-    navbar_desktop.innerHTML+=`<a href="#${navbar_list[i].href}">${navbar_list[i].name}</a>`
+    navbar_desktop.innerHTML+=`<a href="${navbar_list[i].href}">${navbar_list[i].name}</a>`
 }
 // mobile
 for(let i=0;i<navbar_list.length;i++){
@@ -47,8 +47,10 @@ function nav(){
 
 
 // app
+let app = document.getElementById("as_app")
+if(app){
+app.innerHTML+='<ul id="as_app-bar"></ul>'
 let app_bar = document.getElementById("as_app-bar")
-if(app_bar){
     for(let b=0;b<as_app_bar.length;b++){
         app_bar.innerHTML+=`<li>
         <a href="#${as_app_bar[b].href}">
@@ -57,4 +59,42 @@ if(app_bar){
         </a>
         </li>`   
     }
+}
+
+
+// errors
+let error = document.getElementById("as_error")
+if(error){
+    error.innerHTML=`
+    <!-- error 404 -->
+    <div id="as_error404" class="as_error">
+        <img src="./img/error404.svg" alt="Aziz Sobirov Error Page">
+        <div class="text">
+            <h2>Page Not Found</h2>
+            <p>We can't find the page you're looking for. You can either return to the previous page, visit or contact our support team.</p>
+            <div class="btn">
+                <a href="${error404_home}">Visit Home Page</a>
+                <a href="${error404_contact}">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- error 503 -->
+    <div id="as_error503" class="as_error">
+        <img src="./img/error503.svg" alt="Aziz Sobirov Error Page">
+        <div class="text">
+            <h2>503 Service Temporarily Unavailable</h2>
+            <p>The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.</p>
+        </div>
+    </div>
+
+    <!-- error 400 -->
+    <div id="as_error400" class="as_error">
+        <img src="./img/error400.svg" alt="Aziz Sobirov Error Page">
+        <div class="text">
+            <h2>404. That's an Error</h2>
+            <p>Your client has issued a malformed or illegal request. That's all we know.</p>
+        </div>
+    </div>
+    `
 }
--- js/index.js
+++ js/index.js
@@ -2,43 +2,48 @@
 // let navbar_list = [
 //     {
 //         name:"Home",
-//         href:"home"
+//         href:"#home"
 //     },{
 //         name:"Resume",
-//         href:"resume"
+//         href:"#resume"
 //     },{
 //         name:"Projects",
-//         href:"projects"
+//         href:"#projects"
 //     },{
 //         name:"Services & Pricing",
-//         href:"section3"
+//         href:"#section3"
 //     },{
 //         name:"Contact",
-//         href:"section4"
+//         href:"#section4"
 //     }
 //     ]
 
 
-let as_app_bar = [
-    {
-        name:"Home",
-        icon:"ai-home",
-        href:"as_app-home"
-    },{
-        name:"Profile",
-        icon:"ai-person",
-        href:"as_app-profile"
-    },{
-        name:"Settings",
-        icon:"ai-gear",
-        href:"as_app-settings"
-    },{
-        name:"Home",
-        icon:"ai-home",
-        href:"as_app-home"
-    },{
-        name:"Profile",
-        icon:"ai-person",
-        href:"as_app-profile"
-    }
-]
+// let as_app_bar = [
+//     {
+//         name:"Home",
+//         icon:"ai-home",
+//         href:"home"
+//     },{
+//         name:"Features",
+//         icon:"far fa-box",
+//         href:"features"
+//     },{
+//         name:"About",
+//         icon:"ai-person",
+//         href:"about"
+//     },{
+//         name:"Contact",
+//         icon:"far fa-user-headset",
+//         href:"contact"
+//     }
+// ]
+
+
+// error 404
+// let error404_home = 'https://omusic.netlify.app'
+// let error404_contact = 'https://omusic.netlify.app'
+
+// window.addEventListener("click",function(){
+//     document.getElementById("as_error404").style.display="flex"
+// })
